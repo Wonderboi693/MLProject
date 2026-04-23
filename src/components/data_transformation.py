e48521b0e493c424312b6645742347dbbd46b8a5
Wonderboi693 <nlmhuy0101@gmail.com>
2026-04-23 17:49:32 +0700

    Model Trainer

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -76,18 +76,17 @@ class DataTransformation:
             preprocessing_obj = self.get_transformer_object()
 
             target_column_name = "math_score"
-            numerical_columns = ["writing_score", "reading_score"]
 
             input_feature_train_df = train_df.drop(columns=target_column_name)
             target_feature_train_df = train_df[target_column_name]
 
-            input_feature_test_df = train_df.drop(columns=target_column_name)
-            target_feature_test_df = train_df[target_column_name]
+            input_feature_test_df = test_df.drop(columns=target_column_name)
+            target_feature_test_df = test_df[target_column_name]
 
             logging.info("Applying preprocessing object on training dataframe and testing dataframe.")
 
             input_feature_train_arr = preprocessing_obj.fit_transform(input_feature_train_df)
-            input_feature_test_arr = preprocessing_obj.transform(input_feature_train_df)
+            input_feature_test_arr = preprocessing_obj.transform(input_feature_test_df)
 
             train_arr = np.c_[input_feature_train_arr, np.array(target_feature_train_df)]
             test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
